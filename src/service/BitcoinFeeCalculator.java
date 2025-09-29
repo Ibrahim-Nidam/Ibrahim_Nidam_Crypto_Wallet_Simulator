4a88ba1d980819a18864f93263fef517859b9620
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-09-29 14:06:17 +0100

    CWS-46 refactor(patterns): add interfaces #done

--- src/service/BitcoinFeeCalculator.java
+++ src/service/BitcoinFeeCalculator.java
@@ -1,5 +1,21 @@
 package service;
 
+import model.Transaction;
+
 public class BitcoinFeeCalculator implements FeeCalculator {
-    
+    private int sizeBytes;
+    private double satoshiPerByte;
+
+    public BitcoinFeeCalculator(int sizeBytes, double satoshiPerByte){
+        this.sizeBytes = sizeBytes;
+        this.satoshiPerByte = satoshiPerByte;
+    }
+
+    public int getSizeBytes(){ return sizeBytes; }
+    public double getSatoshiPerByte(){ return satoshiPerByte; }
+
+    @Override
+    public double calculateFee(Transaction tx){
+        return sizeBytes * satoshiPerByte;
+    } 
 }
--- src/service/FeeCalculator.java
+++ src/service/FeeCalculator.java
@@ -1,5 +1,7 @@
 package service;
 
+import model.Transaction;
+
 public interface FeeCalculator {
-    
+    double calculateFee(Transaction tx);
 }
