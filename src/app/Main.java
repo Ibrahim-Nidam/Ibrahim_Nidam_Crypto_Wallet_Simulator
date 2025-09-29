5187b373d29d939790661602b22c67e929aacecd
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-09-29 15:37:41 +0100

    feat(app): add Main.java entry point

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,12 @@
 package app;
 
+import ui.Menu;
+import ui.MenuFactory;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Menu menu = MenuFactory.createMenu();
+        menu.start();
+    }
 }
