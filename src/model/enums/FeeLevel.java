92cdfffaacfc04b2829f426204f18f1d603c9e64
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-09-29 14:53:09 +0100

    CWS-12 feat(transaction): implement Priority enum (ECONOMIC, STANDARD, FAST). #done

--- src/model/enums/FeeLevel.java
+++ src/model/enums/FeeLevel.java
@@ -1,5 +1,18 @@
 package model.enums;
 
 public enum FeeLevel {
-    
+    ECONOMIQUE(1, "Transaction lente, frais faible"),
+    STANDARD(2, "Transaction normale, frais moyens"),
+    RAPID(3, "Transaction rapide, frais élevés");
+
+    private final int multiplier;
+    private final String description;
+
+    FeeLevel(int multiplier, String description){
+        this.multiplier = multiplier;
+        this.description = description;
+    }
+
+    public int getMultiplier(){ return multiplier; }
+    public String getDescription(){ return description; }
 }
